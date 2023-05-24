82759f79c724ee5014bd864792c19630fb3f8ad8
c2coder <=>
2023-05-24 19:11:42 +0200

    update

--- server/server.py
+++ server/server.py
@@ -34,6 +34,7 @@ chars = ["a", "b", "c", "d", "e", "f", "g", "h",
 
 colors = ['white', 'platinum', 'grey', 'black', 'pink', 'red', 'orange',
           'brown', 'yellow', 'lime', 'green', 'cyan', 'lblue', 'blue', 'mauve', 'purple']
+
 hex_colors = ['#FFFFFF', '#E4E4E4', '#888888', '#222222', '#FFA7D1', '#E50000', '#E59500',
               '#A06A42', '#E5D900', '#94E044', '#02BE01', '#00D3DD', '#0083C7', '#0000EA', '#CF6EE4', '#820080']
 
